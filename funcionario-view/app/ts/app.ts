46ff1619f74d764ab5ccf14c59c95bd504589377
igordev <igorth.genesis@gmail.com>
2020-08-19 14:48:10 -0300

    import jQuery

--- funcionario-view/app/ts/app.ts
+++ funcionario-view/app/ts/app.ts
@@ -1,6 +1,7 @@
 import { FuncionarioController } from './controllers/FuncionarioController'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/dist/modal.js'
 
 
 const controller = new FuncionarioController();
--- funcionario-view/webpack.config.js
+++ funcionario-view/webpack.config.js
@@ -1,12 +1,22 @@
 const path = require('path');
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
 
 let plugins = [];
 
 plugins.push(new extractTextPlugin("styles.css"));
 
+plugins.push(
+    new webpack.ProvidePlugin({
+        '$' : 'jquery/dist/jquery.js',
+        'jQuery' : 'jquery/dist/jquery.js'
+    })
+);
+
 if (process.env.NODE_ENV === 'production') {
+
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
 
     plugins.push(new optimizeCSSAssetsPlugin({
         cssProcessor: require('cssnano'),
