76e4062e6ae8c12fc4e902f22880ecec4784010d
igordev <igorth.genesis@gmail.com>
2020-08-21 10:41:08 -0300

    correção ";" no final do retorno

--- funcionario-view/app/ts/views/DialogoConfirmacaoView.ts
+++ funcionario-view/app/ts/views/DialogoConfirmacaoView.ts
@@ -23,7 +23,7 @@ export class DialogoConfirmacaoView extends View<string> {
                     </div>
                     </div>
                 </div>
-                </div>`
+                </div>`;
     }
     
     mostra(mensagem: string, acaoConfirma: AcaoConfirma) {
